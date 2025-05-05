f000702ea64509550c28f1981e9743fd443c8663
alospinap <alospinap@unal.edu.co>
2025-05-05 12:16:41 -0500

    funcion init

--- java.js
+++ java.js
@@ -1,3 +1,14 @@
 function init(){
+    var card = document.getElementById("card1")
+    card = card.innerHTML=
+    `
+        <div>
+            <h1> Lista de productos</h1> 
+            <h4>Producto A</h4>
+        </div>
     
+    
+    
+    `
+
 } 
